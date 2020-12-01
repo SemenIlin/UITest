e7aefbeb3d3bbc29cf4bf7aac68608990aa280b9
semen_i <semen_i@holdingbp.ru>
2020-12-01 21:17:32 +0300

    add button controller

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] private GameObject openButton;
+    [SerializeField] private GameObject UI;
+
+    public void CloseUI() 
+    {
+        UI.SetActive(false);
+        openButton.SetActive(true);
+    }
+
+    public void OpenUI()
+    {
+        UI.SetActive(true);
+        openButton.SetActive(false);
+    }
+}
